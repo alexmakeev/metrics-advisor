ca9359a8c74b5a5f8cde9b3055cb4ae4b2ed78be
Alexander Makeev <alexander.v.makeev@gmail.com>
2017-02-08 12:01:46 +0500

    Up

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var NARODNAYA_SAJEN = 176.0;
 var FI = Math.pow(Math.pow(2, 25), 1.0 / 36.0);
 var best_vurf = 1.341669202;
 var precision = 0.03;
-var vurf_delta = best_vurf * precision/4; // 3%
+var vurf_delta = best_vurf * precision; // 3%
 var good_quantities = [2, 4, 6, 8];
 console.log("NARODNAYA_SAJEN =", NARODNAYA_SAJEN);
 console.log("FI =", FI);
@@ -41,8 +41,9 @@ function is_good_vurf(a, b, c) {
 }
 
 function is_good_length(length, sajen_length) {
-  for (var i in good_quantities) {
-    if (Math.abs(length - sajen_length * good_quantities[i]) < length * precision) return true;
+  if ((length > sajen_length) && (length % sajen_length < length * precision) && (Math.floor(length / sajen_length) <= 4)) {
+    console.log('Good:', length, sajen_length, Math.floor(length / sajen_length));
+    return true;
   }
   return false;
 }
@@ -65,90 +66,68 @@ $(function () {
 
     // Here, we're passing in some initial data
     data: function () {
+      var heights = [];
+      for (var i=0; i<20; i++){
+        heights.push(Math.round(247 * Math.pow(1.05943508, i)));
+      }
       return {
-        height: 280.0,
-        sajeni: [],
-        variants: []
+        heights: heights,
+        height: 247,
+        variants: [],
+        is_thin_room: false,
+        is_high_room: false,
+        selected_str: 'Кликнете на комнату чтобы выбрать ее размер.'
       }
     },
 
     oninit: function () {
       var self = this;
+      
+      self.observe("height is_thin_room is_high_room", function () {
 
-
-      self.observe("height", function (new_height) {
-        var triedinstva = self.get("sajeni");
-        var variants = self.get("variants");
-        triedinstva.splice(0, triedinstva.length);
-        variants.splice(0, variants.length);
-        var sajeni_ra = [];
-        var sajeni_svetoch = [];
-        var sajeni_ustye = [];
-
-        for (var i = -30; i += 1; i < 43) {
-          var s1_ra = new Sajen(1, i);
-          var s2_svetoch = new Sajen(0, i);
-          var s3_ustye = new Sajen(-1, i);
-          if (is_good_length(new_height, s1_ra.length)) {
-            sajeni_ra.push(s1_ra);
-          }
-          sajeni_svetoch.push(s2_svetoch);
-          sajeni_ustye.push(s3_ustye);
-        }
-
-        // console.log(sajeni_ra)
-        // console.log(sajeni_svetoch)
-        // console.log(sajeni_ustye)
-
-        for (var i1 in sajeni_ra) {
-          var s1 = sajeni_ra[i1];
-          for (var i2 in sajeni_svetoch) {
-            var s2 = sajeni_svetoch[i2];
-            for (var i3 in sajeni_ustye) {
-              var s3 = sajeni_ustye[i3];
-              if (is_good_vurf(s1.length, s2.length, s3.length)) {
-                triedinstva.push([s1, s2, s3]);
+        var variants = [];
+        var height = self.get('height');
+        var saj_variants = [[164, 134], [134, 164]];
+        var saj_muls = [[1, 2], [2, 3], [4, 4], [4, 5], [4, 6], [4, 7]];
+        // 1:2, 2:3, 4:4 или 4:5. Также гармоничными пропорциями считаются 4:6 и 4:7
+        for (var saj_var_i in saj_variants) {
+          var saj_width = saj_variants[saj_var_i][0];
+          var saj_length = saj_variants[saj_var_i][1];
+          for (var scale = -5; scale <= 5; scale += 1) {
+            saj_width *= Math.pow(1.05943508, scale);
+            saj_length *= Math.pow(1.05943508, scale);
+            
+            for (var saj_scale_height_width_i in saj_muls) {
+              var width = 0;
+              if (self.get('is_thin_room')){
+                width = height * saj_muls[saj_scale_height_width_i][0] / saj_muls[saj_scale_height_width_i][1]; // height is larger than width
+              } else {
+                width = height * saj_muls[saj_scale_height_width_i][1] / saj_muls[saj_scale_height_width_i][0]; // width is larger than height
               }
-            }
-          }
-        }
-
-        // console.log(triedinstva)
-
-        for (var i in triedinstva) {
-          var triedinstvo = triedinstva[i];
-
-          for (var i0 in good_quantities) {
-            if (Math.abs(new_height - good_quantities[i0] * triedinstvo[0].length) > new_height * precision) continue;
-            for (var i1 in good_quantities) {
-              for (var i2 in good_quantities) {
-                var vurf = get_vurf(good_quantities[i0] * triedinstvo[0].length, good_quantities[i1] * triedinstvo[1].length, good_quantities[i2] * triedinstvo[2].length);
-                var svurf = get_vurf(triedinstvo[0].length, triedinstvo[1].length, triedinstvo[2].length);
-                if (is_good_vurf(good_quantities[i0] * triedinstvo[0].length, good_quantities[i1] * triedinstvo[1].length, good_quantities[i2] * triedinstvo[2].length)){
-                  if (good_quantities[i1] * triedinstvo[1].length > good_quantities[i2] * triedinstvo[2].length) {
-                    variants.push([good_quantities[i0] * triedinstvo[0].length, good_quantities[i1] * triedinstvo[1].length, good_quantities[i2] * triedinstvo[2].length, vurf, svurf]);
-                  } else {
-                    variants.push([good_quantities[i0] * triedinstvo[0].length, good_quantities[i2] * triedinstvo[2].length, good_quantities[i1] * triedinstvo[1].length, vurf, svurf]);
-                  }
+              // if (!is_good_length(width, saj_width)) continue;
+              for (var saj_scale_width_length_i in saj_muls) {
+                var length = 0;
+                if (self.get('is_high_room')) {
+                  length = height * saj_muls[saj_scale_width_length_i][0] / saj_muls[saj_scale_width_length_i][1]; // height is larger than length
+                } else {
+                  length = width * saj_muls[saj_scale_width_length_i][1] / saj_muls[saj_scale_width_length_i][0]; // length is larger than width
                 }
+                // if (!is_good_length(length, saj_length)) continue;
+                if (!is_good_vurf(height, width, length)) continue;
+                variants.push([height, length, width, get_vurf(height, width, length), scale, saj_muls[saj_scale_height_width_i], saj_muls[saj_scale_width_length_i]])
               }
             }
           }
         }
-        console.log(variants)
+
         variants.sort(function (a, b) {
           if (a[1] == b[1]) return (-a[2] + b[2]);
           return -a[1] + b[1];
         });
 
-        self.set("variants", variants)
-
-        // console.log(get_vurf(s1.length, s2.length, s3.length));
-        // console.log(s1.length * 2, s1.length * 4, s1.length * 6);
-        //
-        // console.log(triedinstva);
+        self.set('variants', variants);
       })
-    }
+    },
 
   });
 });
